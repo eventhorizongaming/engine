f146483e881bd5bf46d21f9f3e2ee01a8421430c
Eli <136011323+EliasElliotson@users.noreply.github.com>
2024-10-25 18:56:38 +0000

    fix: added the tileset displayer to the renderer exporter

--- public/js/renderers/pixi/index.js
+++ public/js/renderers/pixi/index.js
@@ -1,6 +1,7 @@
 import { Tilemap } from './src/tiles/Tilemap';
 import { Tileset } from './src/tiles/Tileset';
+import { TilesetDisplay } from './src/tiles/TilesetDisplay';
 import * as PIXI from 'pixi.js';
 import * as TILEMAPS from '@pixi/tilemap';
 
-export { Tilemap, Tileset, PIXI, TILEMAPS }
+export { Tilemap, Tileset, TilesetDisplay, PIXI, TILEMAPS }
