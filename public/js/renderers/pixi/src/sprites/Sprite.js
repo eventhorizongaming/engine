20b59aeb44f19194616641f5bc8da0cdf63dfc78
Eli <136011323+EliasElliotson@users.noreply.github.com>
2024-10-29 23:42:34 +0000

    feat: created a sprite utility that uses the new spritesheet class

---
+++ public/js/renderers/pixi/src/sprites/Sprite.js
@@ -0,0 +1,42 @@
+import * as PIXI from 'pixi.js';
+
+export class Sprite extends PIXI.Sprite {
+  spritesheet;
+  currentSprite;
+  currentFrame = 0;
+
+  constructor(spritesheet) {
+    super(spritesheet.texture.clone());
+    this.spritesheet = spritesheet;
+
+    // Pick a default sprite
+    this.currentSprite = Object.keys(spritesheet.sprites)[0];
+
+    // Update the texture's framing when it's loaded
+    this.texture.baseTexture.on("loaded", () => {
+      this.updateTextureFraming();
+    })
+  }
+
+  setSprite(name) {
+    this.currentSprite = name;
+    this.currentFrame = 0;
+    this.updateTextureFraming();
+  }
+
+  updateTextureFraming() {
+    if (this.texture.valid && this.texture.frame) {
+      const box = this.spritesheet.getSprite(this.currentSprite, this.currentFrame);
+      this.texture.frame = new PIXI.Rectangle(box.x, box.y, box.w, box.h);
+    }
+  }
+
+  /**
+   * Steps the sprite forward in its animation
+   * @param {number} frames The number of frames forward to step in the animation
+   */
+  step(frames = 1) {
+    this.currentFrame += frames;
+    this.updateTextureFraming();
+  }
+}
