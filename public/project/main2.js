7c8a5f753f339f8fcc3ad03e7c358be9b974bb55
Eli <136011323+EliasElliotson@users.noreply.github.com>
2024-11-07 13:41:46 +0000

    fix: created an importless version of the project script

---
+++ public/project/main2.js
@@ -0,0 +1,42 @@
+// Make all textures look pixelated
+PIXI.BaseTexture.defaultOptions.scaleMode = 0;
+
+// Create a new pixi.js app
+const app = new PIXI.Application({ resizeTo: window });
+globalThis.__PIXI_APP__ = app;
+
+// Add the app to the dom
+document.body.appendChild(app.view);
+
+// Load in a dancing bush tileset
+const testTileset = await Tileset.from('/resources/tilesets/tileset.json');
+
+// Create a new tilemap display
+const tilemap = new TilesetDisplay(testTileset);
+
+// Upscale the tilemap
+tilemap.scale.x = 8;
+tilemap.scale.y = 8;
+
+// Add the tilemap to the stage
+app.stage.addChild(tilemap);
+
+// Load in the mario spritesheet
+const spritesheet = await SpriteSheet.from('/resources/spritesheets/spritesheet.json');
+
+// Create a Mario sprite
+const mario = new Sprite(spritesheet);
+mario.setSprite('walk')
+
+mario.position.y = 128;
+mario.scale.x = 8;
+mario.scale.y = 8;
+
+app.stage.addChild(mario);
+
+// Update the assets every 16 frames
+app.ticker.add((delta) => {
+  tilemap.step(delta / 16);
+
+  mario.step(delta / 16);
+});
